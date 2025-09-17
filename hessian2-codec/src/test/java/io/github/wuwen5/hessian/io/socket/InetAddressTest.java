cc76aedb2436c28c453846165a98d1696ab8f7b3
wuwen <wuwen.55@aliyun.com>
2025-09-17 23:59:35 +0800

    fix ut

--- hessian2-codec/src/test/java/io/github/wuwen5/hessian/io/socket/InetAddressTest.java
+++ hessian2-codec/src/test/java/io/github/wuwen5/hessian/io/socket/InetAddressTest.java
@@ -17,10 +17,13 @@
 package io.github.wuwen5.hessian.io.socket;
 
 import io.github.wuwen5.hessian.io.SerializeTestBase;
+import io.vavr.control.Try;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -31,44 +34,46 @@ public class InetAddressTest extends SerializeTestBase {
     @Test
     void testInetAddress() throws Exception {
         Inet4Address inet4Address = (Inet4Address) Inet4Address.getByAddress("baidu.com", new byte[] {1, 2, 3, 4});
-        Assertions.assertEquals(inet4Address, baseHessian2Serialize(inet4Address));
-        Assertions.assertEquals(
-                inet4Address.getHostAddress(),
-                baseHessian2Serialize(inet4Address).getHostAddress());
-        Assertions.assertEquals(
-                inet4Address.getHostName(), baseHessian2Serialize(inet4Address).getHostName());
-        Assertions.assertArrayEquals(
-                inet4Address.getAddress(), baseHessian2Serialize(inet4Address).getAddress());
+        Inet4Address serialize = baseHessian2Serialize(inet4Address);
+        Assertions.assertEquals(inet4Address, serialize);
+        Assertions.assertEquals(inet4Address.getHostAddress(), serialize.getHostAddress());
+        Assertions.assertEquals(inet4Address.getHostName(), serialize.getHostName());
+        Assertions.assertArrayEquals(inet4Address.getAddress(), serialize.getAddress());
 
         InetAddress inetAddress = Inet6Address.getByAddress(
                 "baidu.com", new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
-        Assertions.assertEquals(inetAddress, baseHessian2Serialize(inetAddress));
-        Assertions.assertEquals(
-                inetAddress.getHostAddress(), baseHessian2Serialize(inetAddress).getHostAddress());
-        Assertions.assertEquals(
-                inetAddress.getHostName(), baseHessian2Serialize(inetAddress).getHostName());
-        Assertions.assertArrayEquals(
-                inetAddress.getAddress(), baseHessian2Serialize(inetAddress).getAddress());
+        InetAddress inetAddress1 = baseHessian2Serialize(inetAddress);
+        Assertions.assertEquals(inetAddress, inetAddress1);
+        Assertions.assertEquals(inetAddress.getHostAddress(), inetAddress1.getHostAddress());
+        Assertions.assertEquals(inetAddress.getHostName(), inetAddress1.getHostName());
+        Assertions.assertArrayEquals(inetAddress.getAddress(), inetAddress1.getAddress());
 
         Inet6Address inet6Address = Inet6Address.getByAddress(
                 "baidu.com", new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}, 10);
-        Assertions.assertEquals(inet6Address, baseHessian2Serialize(inet6Address));
-        Assertions.assertEquals(
-                inet6Address.getHostAddress(),
-                baseHessian2Serialize(inet6Address).getHostAddress());
-        Assertions.assertEquals(
-                inet6Address.getHostName(), baseHessian2Serialize(inet6Address).getHostName());
-        Assertions.assertArrayEquals(
-                inet6Address.getAddress(), baseHessian2Serialize(inet6Address).getAddress());
+        Inet6Address inet6Address1 = baseHessian2Serialize(inet6Address);
+        Assertions.assertEquals(inet6Address, inet6Address1);
+        Assertions.assertEquals(inet6Address.getHostAddress(), inet6Address1.getHostAddress());
+        Assertions.assertEquals(inet6Address.getHostName(), inet6Address1.getHostName());
+        Assertions.assertArrayEquals(inet6Address.getAddress(), inet6Address1.getAddress());
+    }
 
-        NetworkInterface networkInterface =
-                NetworkInterface.getNetworkInterfaces().nextElement();
-        inet6Address = Inet6Address.getByAddress(
-                "baidu.com", InetAddress.getByName("::1").getAddress(), networkInterface);
-        Inet6Address result = baseHessian2Serialize(inet6Address);
-        Assertions.assertEquals(inet6Address, result);
-        Assertions.assertEquals(inet6Address.getHostAddress(), result.getHostAddress());
-        Assertions.assertEquals(inet6Address.getHostName(), result.getHostName());
-        Assertions.assertArrayEquals(inet6Address.getAddress(), result.getAddress());
+    @Test
+    void testGetByAddressWithNetworkInterface() throws Exception {
+        Optional<NetworkInterface> optNif = Collections.list(NetworkInterface.getNetworkInterfaces()).stream()
+                .filter(t -> Try.of(t::isUp).get())
+                .filter(nif -> Collections.list(nif.getInetAddresses()).stream()
+                        .anyMatch(address -> address instanceof Inet6Address))
+                .findFirst();
+
+        if (optNif.isPresent()) {
+            NetworkInterface nif = optNif.get();
+            Inet6Address inet6Address = Inet6Address.getByAddress(
+                    "baidu.com", InetAddress.getByName("::1").getAddress(), nif);
+            Inet6Address result = baseHessian2Serialize(inet6Address);
+            Assertions.assertEquals(inet6Address, result);
+            Assertions.assertEquals(inet6Address.getHostAddress(), result.getHostAddress());
+            Assertions.assertEquals(inet6Address.getHostName(), result.getHostName());
+            Assertions.assertArrayEquals(inet6Address.getAddress(), result.getAddress());
+        }
     }
 }
