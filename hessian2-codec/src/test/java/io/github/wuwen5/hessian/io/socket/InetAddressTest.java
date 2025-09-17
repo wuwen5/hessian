8e952562db67a0d33004b7957a98fc339b817ca0
wuwen <wuwen.55@aliyun.com>
2025-09-17 23:28:11 +0800

    feat(InetAddress): Supports IPv6

--- hessian2-codec/src/test/java/io/github/wuwen5/hessian/io/socket/InetAddressTest.java
+++ hessian2-codec/src/test/java/io/github/wuwen5/hessian/io/socket/InetAddressTest.java
@@ -20,17 +20,16 @@ import io.github.wuwen5.hessian.io.SerializeTestBase;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class InetAddressTest extends SerializeTestBase {
+
     /**
-     * TODO
      * */
     @Test
-    @Disabled
-    void testJdk8() throws Exception {
+    void testInetAddress() throws Exception {
         Inet4Address inet4Address = (Inet4Address) Inet4Address.getByAddress("baidu.com", new byte[] {1, 2, 3, 4});
         Assertions.assertEquals(inet4Address, baseHessian2Serialize(inet4Address));
         Assertions.assertEquals(
@@ -61,40 +60,15 @@ public class InetAddressTest extends SerializeTestBase {
                 inet6Address.getHostName(), baseHessian2Serialize(inet6Address).getHostName());
         Assertions.assertArrayEquals(
                 inet6Address.getAddress(), baseHessian2Serialize(inet6Address).getAddress());
-    }
 
-    @Test
-    void testJdk17() throws Exception {
-        Inet4Address inet4Address = (Inet4Address) Inet4Address.getByAddress("baidu.com", new byte[] {1, 2, 3, 4});
-        Assertions.assertEquals(inet4Address, baseHessian2Serialize(inet4Address));
-        Assertions.assertEquals(
-                inet4Address.getHostAddress(),
-                baseHessian2Serialize(inet4Address).getHostAddress());
-        Assertions.assertEquals(
-                inet4Address.getHostName(), baseHessian2Serialize(inet4Address).getHostName());
-        Assertions.assertArrayEquals(
-                inet4Address.getAddress(), baseHessian2Serialize(inet4Address).getAddress());
-
-        InetAddress inetAddress = Inet6Address.getByAddress(
-                "baidu.com", new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
-        Assertions.assertEquals(inetAddress, baseHessian2Serialize(inetAddress));
-        Assertions.assertEquals(
-                inetAddress.getHostAddress(), baseHessian2Serialize(inetAddress).getHostAddress());
-        Assertions.assertEquals(
-                inetAddress.getHostName(), baseHessian2Serialize(inetAddress).getHostName());
-        Assertions.assertArrayEquals(
-                inetAddress.getAddress(), baseHessian2Serialize(inetAddress).getAddress());
-
-        Inet6Address inet6Address = Inet6Address.getByAddress(
-                "baidu.com", new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}, 10);
-        Assertions.assertEquals(inet6Address, baseHessian2Serialize(inet6Address));
-        // TODO
-        //        Assertions.assertEquals(
-        //                inet6Address.getHostAddress(),
-        //                baseHessian2Serialize(inet6Address).getHostAddress());
-        Assertions.assertEquals(
-                inet6Address.getHostName(), baseHessian2Serialize(inet6Address).getHostName());
-        Assertions.assertArrayEquals(
-                inet6Address.getAddress(), baseHessian2Serialize(inet6Address).getAddress());
+        NetworkInterface networkInterface =
+                NetworkInterface.getNetworkInterfaces().nextElement();
+        inet6Address = Inet6Address.getByAddress(
+                "baidu.com", new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}, networkInterface);
+        Inet6Address result = baseHessian2Serialize(inet6Address);
+        Assertions.assertEquals(inet6Address, result);
+        Assertions.assertEquals(inet6Address.getHostAddress(), result.getHostAddress());
+        Assertions.assertEquals(inet6Address.getHostName(), result.getHostName());
+        Assertions.assertArrayEquals(inet6Address.getAddress(), result.getAddress());
     }
 }
